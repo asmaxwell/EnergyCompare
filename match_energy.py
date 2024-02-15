b0a361ce0667dbfb23d0f0d15057489f3e3f6873
Andrew Maxwell <andrew.maxwell@ucl.ac.uk>
2024-02-15 12:20:32 +0000

    Working Script

---
+++ match_energy.py
@@ -0,0 +1,63 @@
+"""
+Created by Andy S Maxwell 14/02/2024
+Script to find matching energy from different line lists
+"""
+
+import pandas as pd
+
+def match_single_energy(tag :str, energy : float, energy_col : int, match_data : pd.DataFrame) -> pd.Series:
+    if tag in match_data.index:
+        #find matching state/ tag
+        partial_data = match_data.loc[tag,:].reset_index(drop=True)    
+        #if only one line matches energy level immeditately return
+        if len(partial_data.shape) < 2:
+            return partial_data
+        
+        #find nearest energy
+        nearest_index = partial_data.loc[:,energy_col].sub(energy).abs().idxmin()
+        return partial_data.loc[nearest_index,:].rename(tag)
+    else:
+        return None
+    
+def combine_rows(Marvel_row : pd.Series, match_data : pd.DataFrame, Marvel_energy_col : int, ExMol_energy_col : int) -> pd.Series:
+    tag = Marvel_row.name
+    energy = Marvel_row[Marvel_energy_col]
+    return pd.concat([Marvel_row, match_single_energy(tag, energy, ExMol_energy_col, match_data)], axis=0).rename(tag)
+
+
+def main() -> None:
+    data_path = "data/"
+    Marvel_filename = "MARVEL_out_tag_nosym" 
+    ExoMol_filename = "C2H2_states_cut_sort_tag_nosym"
+    out_filename = Marvel_filename + "_" + ExoMol_filename
+
+    Marvel_tag_col = 15
+    ExMol_tag_col = 20
+    Marvel_energy_col = 12
+    ExMol_energy_col = 1
+
+    print("Loading data")
+    Marvel_data = pd.read_csv(data_path + Marvel_filename, delim_whitespace=True, header=None, index_col=Marvel_tag_col
+                              , nrows=5000
+                              )
+    ExMol_data = pd.read_csv(data_path + ExoMol_filename, delim_whitespace=True, header=None, index_col=ExMol_tag_col
+                             , nrows=50000
+                             )
+
+    print("Matching data")
+    out_df = Marvel_data.apply(lambda x : combine_rows(x, ExMol_data, Marvel_energy_col, ExMol_energy_col), axis=1 )
+    print("Example of matched energies:")
+    print(out_df.iloc[:,[12,16]])
+    out_df.to_csv(data_path + out_filename, sep=' ', header=False)
+    
+
+    return
+
+#When script is run from the command line it runs this part
+if __name__ == "__main__":
+    main()
+
+
+    
+
+
---
+++ test_match_energy.py
@@ -0,0 +1,58 @@
+import pytest
+import match_energy
+import numpy as np
+import pandas as pd\
+
+@pytest.fixture(scope='session') 
+def load_files():
+    data_path = "data/"
+    Marvel_filename = "MARVEL_out_tag_nosym" 
+    ExoMol_filename = "C2H2_states_cut_sort_tag_nosym"
+
+    Marvel_data = pd.read_csv(data_path + Marvel_filename, delim_whitespace=True, header=None, index_col=15, nrows=250)
+    ExMol_data = pd.read_csv(data_path + ExoMol_filename, delim_whitespace=True, header=None, index_col=20, nrows=5000)
+    yield Marvel_data, ExMol_data 
+    return
+
+
+def test_match_single_energy(load_files):
+    Marvel_energy_col = 12
+    ExMol_energy_col = 1
+    
+    Marvel_data, ExMol_data = load_files
+    Marvel_row = Marvel_data.iloc[100]
+    tag = Marvel_row.name
+    energy = Marvel_row[Marvel_energy_col]
+    partial_ExMol = match_energy.match_single_energy(tag, energy, ExMol_energy_col, ExMol_data)
+
+    assert(partial_ExMol.name == tag)
+    assert(np.abs(partial_ExMol[1] -energy)/energy < 0.01)
+    return
+
+def test_combine_rows(load_files):
+    Marvel_data, ExMol_data = load_files
+    Marvel_tag_col = 15
+    Marvel_energy_col = 12
+    ExMol_energy_col = 1
+
+    
+    Marvel_row = Marvel_data.iloc[187]
+    tag = Marvel_row.name
+    energy = Marvel_row[Marvel_energy_col]
+    partial_comb = match_energy.combine_rows(Marvel_row, ExMol_data, Marvel_energy_col, ExMol_energy_col)
+
+    assert(partial_comb.name == tag)
+    assert(np.abs(partial_comb.iloc[Marvel_tag_col + ExMol_energy_col] -energy)/energy < 0.01)
+    return
+
+def test_make_new_dataframe(load_files):
+    Marvel_data, ExMol_data = load_files
+    Marvel_tag_col = 15
+    Marvel_energy_col = 12
+    ExMol_energy_col = 1
+    
+    out_df = Marvel_data.apply(lambda x : match_energy.combine_rows(x, ExMol_data, Marvel_energy_col, ExMol_energy_col), axis=1 )
+
+    conditions = np.abs(out_df.iloc[:,Marvel_energy_col] - out_df.iloc[:,Marvel_tag_col+ExMol_energy_col]) / (out_df.iloc[:,Marvel_energy_col] + 1e-12) < 0.01
+    assert(conditions.eq(True).all())
+    return
